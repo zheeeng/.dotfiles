77c855a7856c959d672451d0257751f855291067
Zheeeng <hi@zheeeng.me>
2017-04-21 14:53:35 +0800

    Add dockerized cmds

--- envconfig/bootstrap.sh
+++ envconfig/bootstrap.sh
@@ -3,4 +3,5 @@
 # Source files
 source $ENVCONFIG_PATH/git-aliases.sh
 source $ENVCONFIG_PATH/docker-aliases.sh
+source $ENVCONFIG_PATH/dockerized-cmds.sh
 
---
+++ envconfig/dockerized-cmds.sh
@@ -0,0 +1,2 @@
+alias vue='docker run -it --rm -v "$PWD":"$PWD" -w "$PWD" ebiven/vue-cli vue'
+alias stylus='docker run -it --rm -v "$PWD":"$PWD" -w "$PWD" zheeeng/stylus-cli stylus'
