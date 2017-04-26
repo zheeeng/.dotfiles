98f366289502cc82b2728243ee78f52e684f17a7
Zheeeng <hi@zheeeng.me>
2017-04-26 14:08:17 +0800

    Fix json-server alias

--- envconfig/dockerized-cmds.sh
+++ envconfig/dockerized-cmds.sh
@@ -1,3 +1,3 @@
 alias vue='docker run -it --rm -v "$PWD":/code ebiven/vue-cli vue'
 alias stylus='docker run -it --rm -v "$PWD":/src zheeeng/stylus-cli stylus'
-alias json-server='_() { local port; local paras; port=3000; paras=$@; echo $@; echo $paras; while [[ $1 != "" ]]; do echo $1; case $1 in -p | --port ) shift; port=$1; break;; *) shift;; esac; done; eval "docker run -it --rm -p $port:$port -v $PWD:/data zheeeng/json-server json-server $paras"; }; _'
+alias json-server='_() { local port; local args; port=3000; args=$@; while [[ $1 != "" ]]; do case $1 in -p | --port ) shift; port=$1; break;; *) shift;; esac; done; eval docker run -it --rm -p $port:$port -v $PWD:/data zheeeng/json-server '"\'"'json-server $args'"\'"'; }; _'
