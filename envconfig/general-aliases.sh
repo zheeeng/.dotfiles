dc70dfd5a75c15a01c2180a0218e00b10f4f80a0
Zheeeng <hi@zheeeng.me>
2017-04-28 01:02:14 +0800

    Add cpdir

--- envconfig/general-aliases.sh
+++ envconfig/general-aliases.sh
@@ -12,6 +12,7 @@ alias clll='clear && ll'
 alias clls='clear && ls'
 alias cllsa='clear && lsa'
 alias rmdir='rm -rf'
+alias cpdir='cp -rf'
 alias mkcd='_(){ mkdir $1; cd $1; }; _'
 alias webshare='python -c "import SimpleHTTPServer;SimpleHTTPServer.test()"'
 
